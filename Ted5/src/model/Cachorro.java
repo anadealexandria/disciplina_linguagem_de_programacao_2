4d9a5eae0dfdf96493235c255dd9957b5c8eceec
Ana de Alexandria <anapaula.jp.br@gmail.com>
2021-04-29 00:14:22 -0300

    Resolução parcial Ted5

--- Ted5/src/model/Cachorro.java
+++ Ted5/src/model/Cachorro.java
@@ -27,64 +27,64 @@ public class Cachorro {
 	
 	public int comer(char c) {
 		int energia = cachorro.getEnergia();
-		if (cachorro.getEnergia() <= 50) {
+		
 			if(c =='R' || c == 'r') {
-				energia = cachorro.getEnergia() + 50;
+				energia += 50;
 			}else if(c == 'C' || c == 'c') {
-				energia = cachorro.getEnergia() + 40;
+				energia += 40;
 			}else if(c == 'L' || c == 'l') {
-				energia = cachorro.getEnergia() + 30;
+				energia += 30;
 			}
-		}
+		
 		return energia;
 	}
 	
 	public int brincar (char b) {
 		int energia = cachorro.getEnergia();
-		if (cachorro.getEnergia() >= 40) {
-			if (b == 'B') {
-				energia = cachorro.getEnergia() - 30;
-			}else if (b == 'S') {
-				energia = cachorro.getEnergia() - 20;
-			}else if (b == 'G') {
-				energia = cachorro.getEnergia() - 10;
+		
+			if (b == 'B' || b == 'b') {
+				energia -= 30;
+			}else if (b == 'S' || b == 's') {
+				energia -= 20;
+			}else if (b == 'G' || b == 'g') {
+				energia -= 10;
 			}
-		}
+		
 		return energia;
 	}
 	
-	public boolean podeCruzar(DadosCachorro parceiro) {
+	public boolean podeCruzar(Cachorro parceiro) {
 		boolean idadesParaCruzar = false;
 		boolean mesmaRaca = false;
 		boolean possuiEnergia = false;
 		boolean sexoDiferente = false;
 		
 		if ((cachorro.getIdade() >= 1 && cachorro.getIdade() <= 9) &&
-				(parceiro.getIdade() >= 1 && parceiro.getIdade() <=9)) {
+				(parceiro.getCachorro().getIdade() >= 1 && parceiro.getCachorro().getIdade() <=9)) {
 			idadesParaCruzar = true;
 		}
-		if (cachorro.getRaca().equals(parceiro.getRaca())) {
+		if (cachorro.getRaca().equals(parceiro.getCachorro().getRaca())) {
 			mesmaRaca = true;
 		}
-		if (cachorro.getEnergia() >= 80 && parceiro.getEnergia() >= 80) {
+		if (cachorro.getEnergia() >= 80 && parceiro.getCachorro().getEnergia() >= 80) {
 			possuiEnergia = true;
 		}
-		if (cachorro.getSexo() != (parceiro.getSexo())) {
+		if (cachorro.getSexo() != (parceiro.getCachorro().getSexo())) {
 			sexoDiferente = true;
 		}
 		return idadesParaCruzar && mesmaRaca && possuiEnergia && sexoDiferente; 
 						
 	}
 	
-	public int cruzar(DadosCachorro parceiro) {
+	public int cruzar(Cachorro parceiro) {
 		int energiaTotalUm = cachorro.getEnergia();
-		int energiaTotalDois = parceiro.getEnergia();
+		int energiaTotalDois = parceiro.getCachorro().getEnergia();
 		
 		if (podeCruzar(parceiro)) {
 			energiaTotalUm -= 50;
 			energiaTotalDois -= 50;
 			cachorro.setEnergia(energiaTotalUm);
-			parceiro.setEnergia(energiaTotalDois);
+			parceiro.getCachorro().setEnergia(energiaTotalDois);
 		}
 		Random numeroAleatorio = new Random();
 		int numeroGerado = numeroAleatorio.nextInt(9) + 1;
--- Ted5/src/view/Casa.java
+++ Ted5/src/view/Casa.java
@@ -9,22 +9,27 @@ import model.DadosCachorro;
 public class Casa {
 
 	public static void main(String[] args) {
+	
 		try(Scanner entrada = new Scanner(System.in)){
+		
 		ArrayList<Cachorro> cachorros = new ArrayList<>();
+		
 		
 		boolean flag = true;
 		
 		while(flag) {
-			System.out.print("Bem-vindo ao cadastro de cães:\n1-Cadastrar cão;\n2-Listar cães;\nSair do programa;\n");
+			System.out.print("Bem-vindo ao cadastro de cães:\n1-Cadastrar cão;\n2-Listar cães;\n3- Sair do programa;"
+					+ "\n(SÓ SERÁ ACEITO UMA DAS OPÇÕES ACIMA)\n");
 			int escolha = entrada.nextInt();
 		
 			switch (escolha) {
 				case 1:
 					System.out.print("Qual o primeiro nome do cãozinho?\n");
 					String nome = entrada.next();
-					System.out.print("Qual a raça do cãozinho?\n");
-					String raca = entrada.next();
-					System.out.print("Qual o sexo do cãozinho?\n");
+					System.out.print("Qual o primeiro nome da raça do cãozinho?(SE FOR 'VIRA-LATA' USE A "
+							+ "SIGLA SRD)\n");
+					String raca = entrada.next().toLowerCase();
+					System.out.print("Qual o sexo do cãozinho?(F/M)\n");
 					char sexo = entrada.next().charAt(0);
 					System.out.print("Qual a idade do cãozinho?\n");
 					int idade = entrada.nextInt();
@@ -37,27 +42,103 @@ public class Casa {
 					break;
 					
 				case 2:
+					for(int i = 0 ; i < cachorros.size(); i++) {
+						System.out.print("\nÍndice " + i + " " + cachorros.get(i));
+						System.out.println();
+					}
 					
+					System.out.println("\nSelecione um cãozinho da lista através do índice: ");
+					int caoEscolhido = entrada.nextInt();					
+					
+					manipularCachorro(cachorros.get(caoEscolhido), cachorros );
+					break;
+					
+				case 3:
+					System.exit(0);
+					}
 				}
 		
 			}
 
 		}
-	}
 	
-	public void manipular_cachorro(Cachorro caoAtual, ArrayList<Cachorro> cachorros) {
+
+	
+	public static void manipularCachorro(Cachorro caoAtual, ArrayList<Cachorro> cachorros) {
 		try(Scanner entrada = new Scanner(System.in)){
+		ArrayList<Cachorro> parceiros = new ArrayList<>();
 		
-		System.out.print("\n1- Ver dados do cãozinho;\n2- Alimentar;\n3- Brincar\n4- Cruzar\n5- Menu Inicial");
+				
+		while(true) {
+		
+		System.out.print("\n1- Ver dados do cãozinho;\n2- Alimentar;\n3- Brincar\n4- Cruzar\n5- Menu Inicial"
+				+ "\n(SÓ SERÁ ACEITO UMA DAS OPÇÕES ACIMA)\n");
 		int escolha = entrada.nextInt();
 		switch (escolha) {
 			case 1:
-		}
+				System.out.println("\n" + caoAtual);
+				break;			
+				
+			case 2:
+				if (caoAtual.getCachorro().getEnergia() <= 50) {
+					System.out.print("\nSeu cãozinho precisa comer.\nEscolha o tipo de comida:\n"
+							+ "R- Ração\nC- Carne\nL- Legumes\n(SÓ SERÁ ACEITO UMA DAS OPÇÕES ACIMA)\n");
+					char escolhaComida = entrada.next().charAt(0);
+				
+					System.out.println("A energia do seu cãozinho é: " + caoAtual.comer(escolhaComida));
+				}else {
+					System.out.println("\nSeu cãozinho não precisa comer!!");
+				}
+				break;
+				
+			case 3:
+				if (caoAtual.getCachorro().getEnergia() >= 40) {
+					System.out.print("\nEscolhas uma das opções abaixo:\nB- Buscar bolinha\nS- Saltar\nG- Girar"
+							+ "\n(SÓ SERÁ ACEITO UMA DAS OPÇÕES ACIMA)\n");
+					char escolhaBrincadeira = entrada.next().charAt(0);
+					caoAtual.brincar(escolhaBrincadeira);
+					System.out.println("A energia do seu cãozinho é: " + caoAtual.brincar(escolhaBrincadeira));
+				}else {
+					System.out.println("\nSeu cãozinho não tem energia para brincar!");
+				}
+				break;
+				
+			case 4:
+				
+				for (int i = 0 ; i < cachorros.size() ; i++) {
+					boolean parceirosPossiveis = caoAtual.podeCruzar(cachorros.get(i));
+					
+					if(parceirosPossiveis) {
+						parceiros.add(cachorros.get(i));
+					}
+				}
+				
+				for (int i = 0 ; i < parceiros.size() ; i++){
+					System.out.println("\nOs parceiros disponíveis para cruzar são:\n\n"
+							+ "Índice " + i + " " + parceiros.get(i));
+				}
+				
+				System.out.println("\nQual cãozinho você quer cruzar com o cão atual? (Escolha o índice) ");
+				int caoParaCruzar = entrada.nextInt();
+				
+				System.out.println("A energia do seu cãozinho é: " + caoAtual.cruzar(parceiros.get(caoParaCruzar)));
+				
+				break;
 		
+			case 5:
+				return;
+				}
+			
+			}
 		}
+	}
+	
+	public static String obterDados(Cachorro cachorro) {
+		return cachorro.toString();
 	}
 
 }
+
 
 
 
